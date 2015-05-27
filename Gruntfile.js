fa09743904fa6674700d4bb98e4a61a4a7b0efdd
Tim Pritchard <tim.pritchard@acknowledgement.co.uk>
2015-05-27 17:47:34 +0100

    basic im client

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+/**
+ * Created by timpritchard on 23/11/14.
+ */
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'public/sass',
+                    cssDir: 'public/stylesheets'
+                }
+            }
+        },
+
+        autoprefixer: {
+            no_dest: {
+                src: 'public/stylesheets/*.css' // globbing is also possible here
+            }
+        },
+        browserify:     {
+            /* Roughly equivalent to: 
+            * jsx --watch ./public/javascripts/src/ ./public/javascripts/build/
+            * browserify ./public/javascripts/build/bundle.js > ./public/javascripts/build/bundle-browserified.js
+            */
+            options:      {
+                transform:  [ require('grunt-react').browserify ]
+            },
+            app:          {
+                src:        'public/javascripts/src/bundle.jsx',
+                dest:       'public/javascripts/build/bundle.js'
+            }
+        },
+
+        watch: {
+            scripts: {
+               files: ["public/javascripts/src/*.jsx"],
+               tasks: ["browserify"],
+               options: {
+                   spawn: false,
+               },
+            },
+            css: {
+               files: ["public/**/*.scss"],
+               tasks: ["compass", "autoprefixer"]
+            },
+            options: {
+                livereload: true
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-browserify');
+
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+
+};
---
+++ public/javascripts/src/bundle.jsx
@@ -0,0 +1,37 @@
+var jquery = require('jquery');
+var unique = require('uniq');
+var React = require('react');
+var socket = require('socket.io-client')();
+
+var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+
+console.log(unique(data));
+
+
+// jQuery
+(function($){
+
+	$('form').submit(function(){
+	    socket.emit('chat message', $('#m').val());
+	    $('#m').val('');
+	    return false;
+	});
+
+	socket.on('chat message', function(msg){
+	    $('#messages').append($('<li>').text(msg));
+	});
+
+	// React
+	React.render(
+		<strong>First message!</strong>,
+		document.getElementById('example')
+	);
+
+})(jquery);
+
+
+
+
+	
+
+
