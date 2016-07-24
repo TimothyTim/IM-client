502358c8137b060e2dd8c920b6209205c869f09c
Tim Pritchard <tim.pritchard01@bbc.co.uk>
2016-07-24 13:56:26 +0100

    tidy

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,17 @@
 var jquery = require('jquery');
 var unique = require('uniq');
 var React = require('react');
-
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
 console.log(unique(data));
 
 React.render(
-	<h1>Hello, world!</h1>,
-	document.getElementById('example')
+    <h1>Hello, world!</h1>,
+        document.getElementById('example')
 );
 
 (function($){
-
-	$('button').click(function(){
-	    $('span').css({"color":"red"});
-	});
-
+    $('button').click(function(){
+        $('span').css({"color":"red"});
+    });
 })(jquery);
-
--- public/javascripts/src/bundle.jsx
+++ public/javascripts/src/bundle.jsx
@@ -7,31 +7,22 @@ var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
 console.log(unique(data));
 
-
 // jQuery
 (function($){
 
-	$('form').submit(function(){
-	    socket.emit('chat message', $('#m').val());
-	    $('#m').val('');
-	    return false;
-	});
+    $('form').submit(function(){
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
 
-	socket.on('chat message', function(msg){
-	    $('#messages').append($('<li>').text(msg));
-	});
+    socket.on('chat message', function(msg){
+        $('#messages').append($('<li>').text(msg));
+    });
 
-	// React
-	React.render(
-		<strong>First message!</strong>,
-		document.getElementById('example')
-	);
-
+    // React
+    React.render(
+        <strong>First message!</strong>,
+            document.getElementById('example')
+        );
 })(jquery);
-
-
-
-
-	
-
-
